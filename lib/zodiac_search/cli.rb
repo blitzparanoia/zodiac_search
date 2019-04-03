b2b0f67f9938b4c0847d6f22dbeab3939f83a947
blitzparanoia <bluemilk44@gmail.com>
2019-04-02 22:55:34 -0400

    outline of methods and input test

--- lib/zodiac_search/cli.rb
+++ lib/zodiac_search/cli.rb
@@ -3,7 +3,30 @@
 class ZodiacSearch::CLI
 
   def call
+    list_zodiac
+  end
+
+  def list_zodiac
+    #welcome message, list of signs
     puts "Welcome!"
+  end
+
+  def menu
+    #prompt for user input, insert logic here
+    input = nil
+    puts "Name a sign or pick a number:"
+    input = gets.strip.downcase
+    while input != "exit"
+      case input
+      when "1"
+        puts "first"
+      end
+    end
+
+  end
+
+  def goodbye
+    #message for exit
   end
 
 
