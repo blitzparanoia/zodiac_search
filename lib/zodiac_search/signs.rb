cac7c4cb5cd528096a3d45c7af584a9db7f7afc8
blitzparanoia <bluemilk44@gmail.com>
2019-04-03 18:53:41 -0400

    Scrape methods in place

--- lib/zodiac_search/signs.rb
+++ lib/zodiac_search/signs.rb
@@ -12,10 +12,14 @@ def self.scrape_zodiacs
 #take the name dates rep and traits
   signs_scraped = []
 
-
+  signs_scraped << self.scrape_astrosite
 #from the url
   signs_scraped
+end
 
+def self.scrape_astrosite
+  doc = Nokogiri::HTML(open("https://www.astrology-zodiac-signs.com/"))
+  binding.pry
 end
 
 
