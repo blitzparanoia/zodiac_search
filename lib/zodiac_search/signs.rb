ce982df0edaa9d223aaf69ab7da7321e15522df8
blitzparanoia <bluemilk44@gmail.com>
2019-04-03 09:34:28 -0400

    added a notes.md file, testing Signs class

--- lib/zodiac_search/signs.rb
+++ lib/zodiac_search/signs.rb
@@ -1,7 +1,19 @@
 class ZodiacSearch::Signs
 
   def self.list
-    puts "Info here"
+    puts %q{ Info here:
+    Aries: The Ram (Mar 21-Apr 19)
+    Taurus: The Bull (Apr 20-May 20)
+    Gemini: The Twins (May 21-Jun 20)
+    Cancer: The Crab (June 21-July 22)
+    Leo: The Lion (July 23-Aug 22)
+    Virgo: The Virgin (Aug 23-Sep 22)
+    Libra: The Scales (Sep 23-Oct 22)
+    Scorpio: The Scorpion (Oct 23-Nov 21)
+    Sagittarius: The Archer (Nov 22-Dec 21)
+    Capricorn: The Goat (Dec 22-Jan 19)
+    Aquarius: The Water Bearer (Jan 20-Feb 18)
+    Pisces: The Fish (Feb 19-Mar 20)}
   end
 
 end
