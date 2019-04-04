63877c5e15bfcdc444bbcef6b0fcb4770a12cc28
blitzparanoia <bluemilk44@gmail.com>
2019-04-03 20:55:41 -0400

    Starting scrape

--- lib/zodiac_search/signs.rb
+++ lib/zodiac_search/signs.rb
@@ -11,15 +11,20 @@ def self.scrape_zodiacs
 #working potential methods and info to scrape, The smae format for all 12 signs
 #take the name dates rep and traits
   signs_scraped = []
-
   signs_scraped << self.scrape_astrosite
 #from the url
   signs_scraped
 end
 
 def self.scrape_astrosite
-  doc = Nokogiri::HTML(open("https://www.astrology-zodiac-signs.com/"))
+  doc = Nokogiri::HTML(open("https://labyrinthos.co/blogs/astrology-horoscope-zodiac-signs/list-of-12-zodiac-signs-dates-meanings-symbols"))
   binding.pry
+
+=begin
+doc.css("h2.p1").children.first.text.gsub("Zodiac Sign", "")  #sign
+doc.css("p.p1 strong").children.first.text  #representing
+
+=end
 end
 
 
